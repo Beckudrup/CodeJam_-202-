27b1b58a570490b705bfb2f8f049ec4e7ad3c464
le-kjaerulff <leonard.kjaerulff@gmail.com>
2023-04-25 11:36:29 +0200

    We ridin now

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] int playerSpeed = 1;
+    Transform cylinderTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cylinderTransform = GameObject.Find("Cylinder").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cylinderTransform.Rotate(0, playerSpeed * Time.deltaTime, 0);
     }
 }
