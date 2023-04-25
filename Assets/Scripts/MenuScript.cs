6eef146ecf72940dd3d66499f726b9dd3b1fbb0d
Akaidus <nikolajjensen6854@gmail.com>
2023-04-25 13:04:10 +0200

    Cow_Start

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Camera mainCam;
+    [SerializeField] GameObject startButton;
+    [SerializeField] int camMenu = 0;
+    [SerializeField] int camStart = 70;
+    [SerializeField] float camSpeed;
+    bool buttonPressed;
+    
     void Start()
     {
-        
+        mainCam.fieldOfView = camMenu;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!buttonPressed) return;
+        var newCamSize = Mathf.Lerp(mainCam.fieldOfView, camStart, Time.deltaTime * camSpeed);
+        mainCam.fieldOfView = newCamSize;
+        if (Math.Abs(mainCam.fieldOfView - camStart) > 5) return;
+        buttonPressed = false;
+    }
+
+    public void StartGame()
+    {
+        buttonPressed = true;
+        startButton.SetActive(false);
     }
 }
