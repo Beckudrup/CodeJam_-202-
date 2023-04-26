89530ba8de7437da57469224ce871317d03fa1a5
Akaidus <nikolajjensen6854@gmail.com>
2023-04-26 15:27:21 +0200

    cow go zoooOOOOOOM

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,9 @@ using TMPro;
 public class Player : MonoBehaviour
 {
     Camera mainCam;
-    int maxCamDistance = 150, minCamDistance = 70;
+    int maxCamFOV = 150, minCamFOV = 70;
     Transform cylinderTransform;
-    Animator cowAnimator;
+    Animator cowCamAnimator;
     [SerializeField] TMP_Text moovementSpeed; //Cows speed 
     [SerializeField] GameObject LeftHorn, RightHorn;
     [HideInInspector] public bool leftButton, rightButton;
@@ -18,14 +19,22 @@ public class Player : MonoBehaviour
     float shakeMultiplier = 0.005f;
     [HideInInspector] public float shakeMoveSpeed;
     float shakeThreshold = 2.0f;
+    float shake;
 
 
     // Start is called before the first frame update
     void Awake()
     {
         mainCam = Camera.main;
-        cowAnimator = GetComponent<Animator>();
+        cowCamAnimator = GetComponent<Animator>();
         cylinderTransform = GameObject.Find("Cylinder").GetComponent<Transform>();
+    }
+
+    void LateUpdate()
+    {
+        if (shake > shakeThreshold) { 
+            mainCam.fieldOfView += shakeMoveSpeed * Time.deltaTime;
+        }
     }
 
     // Update is called once per frame
@@ -42,14 +51,17 @@ public class Player : MonoBehaviour
             moovementSpeed.text = playerSpeed.ToString("0.00"); //Kommenteret ud for nu da det kun virker p� mobil
             cylinderTransform.Rotate(0, playerSpeed * Time.deltaTime, 0);
             */
-            var shake = Input.acceleration.magnitude;
-            cowAnimator.SetTrigger("isRiding");
+            shake = Input.acceleration.magnitude;
+            cowCamAnimator.SetTrigger("isRiding"); 
+            cowCamAnimator.cullingMode = AnimatorCullingMode.CullUpdateTransforms;
             if (shake > shakeThreshold) { 
                 shakeMoveSpeed += shake * shakeMultiplier;
+                DistortCamera();
             }
             else
             {
                 shakeMoveSpeed -= shake * (shakeMultiplier+shakeMultiplier);
+                UnDistortCamera();
                 //Godt til threshholds (minder om et if statement) (ser om shakeMoveSpeed er mindre end 0 og s�tter derefter det til 0)
                 shakeMoveSpeed = shakeMoveSpeed < 0 ? 0 : shakeMoveSpeed;
             }
@@ -67,5 +79,19 @@ public class Player : MonoBehaviour
     public void RightButton()
     {
         rightButton = !rightButton;
+    }
+
+    void DistortCamera()
+    {
+        mainCam.fieldOfView += shakeMoveSpeed/5;
+        mainCam.fieldOfView = mainCam.fieldOfView > maxCamFOV ? maxCamFOV : mainCam.fieldOfView;
+        //mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, maxCamFOV, shakeMoveSpeed * Time.deltaTime);
+    }
+
+    void UnDistortCamera()
+    {
+        mainCam.fieldOfView -= shakeMoveSpeed/10;
+        mainCam.fieldOfView = mainCam.fieldOfView < minCamFOV ? minCamFOV : mainCam.fieldOfView;
+        //mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, minCamFOV, shakeMoveSpeed * Time.deltaTime);
     }
 }
