ec6a9722da0bcd615c168b822c929e7c1f2ba917
Slente468 <GL51HW@student.aau.dk>
2023-04-26 14:53:17 +0200

    Chicken sound by K
    Also copied main scene so I can throw chickens around, no problem.

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -12,6 +12,8 @@ public class SoundManager : MonoBehaviour
     [SerializeField] private AudioSource bgm, moo, gunshot, running, cowbell, chicken;
     public static SoundManager Instance;
 
+    
+
     void Awake()
     {
         if (Instance == null)
@@ -25,20 +27,35 @@ public class SoundManager : MonoBehaviour
         }
     }
     public void PlaySound(AudioClip clip)
-        {
-        bgm.PlayOneShot(clip);
-        // moo.PlayOneShot(clip);
+       {
+      bgm.PlayOneShot(clip);
+         //moo.PlayOneShot(clip);
         // gunshot.PlayOneShot(clip);
         // running.PlayOneShot(clip);
         // cowbell.PlayOneShot(clip);
         // chicken.PlayOneShot(clip);
+       }
+
+    /// <summary>
+    /// ES- Should be able to randomize sound effects by taking inspiration from this:
+    /// https://www.youtube.com/watch?v=lqyzGntF5Hw&t=12s
+    ///(not used yet)
+    /// </summary>
+    public void Update()
+    { // ES- input. part is 'press k do chicken sound, //from here the k sould be moved 
+        // to an collider, put script on chicken game object. 
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+                 ChickenSound(chicken.clip);
         }
-    
-        // public void BGMSound(AudioClip clip) => bgm.PlayOneShot(clip);
-        // public void MooSound(AudioClip clip) => moo.PlayOneShot(clip);
-        // public void GunshotSound(AudioClip clip) => gunshot.PlayOneShot(clip);
-        // public void RunningSound(AudioClip clip) => running.PlayOneShot(clip);
-        // public void CowbellSound(AudioClip clip) => cowbell.PlayOneShot(clip);
-        // public void ChickenSound(AudioClip clip) => chicken.PlayOneShot(clip);
-        // public void Gunshot(AudioClip clip) => gunshot.PlayOneShot(clip);
+    }
+
+    // public void BGMSound(AudioClip clip) => bgm.PlayOneShot(clip);
+     //public void MooSound(AudioClip clip) => moo.PlayOneShot(clip);
+    // public void GunshotSound(AudioClip clip) => gunshot.PlayOneShot(clip);
+    // public void RunningSound(AudioClip clip) => running.PlayOneShot(clip);
+     public void CowbellSound(AudioClip clip) => cowbell.PlayOneShot(clip);
+     public void ChickenSound(AudioClip clip) => chicken.PlayOneShot(clip);
+    // public void Gunshot(AudioClip clip) => gunshot.PlayOneShot(clip);
+
 }
