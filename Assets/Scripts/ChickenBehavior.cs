5f5e3efca07fea5975dd24ec62a5b74a23c8276a
Slente468 <GL51HW@student.aau.dk>
2023-05-04 14:00:29 +0200

    commentary

--- Assets/Scripts/ChickenBehavior.cs
+++ Assets/Scripts/ChickenBehavior.cs
@@ -10,6 +10,9 @@ public class ChickenBehavior : MonoBehaviour
         if(other.gameObject.tag == "Player")
         {
             SoundManager.Instance.ChickenSound();
+            // the chicken is carried behind the player aka cow, as a
+            // child object of the cow,therefor the transform.SetParent(Null)
+            // removes it from the hierachy, (so the chicken can go flying.) 
             transform.SetParent(null);
             hitByCow = true;
             Invoke("RemoveAfterHit", chickenLifeTime);
@@ -24,8 +27,13 @@ public class ChickenBehavior : MonoBehaviour
         }
     }
 
+
+    // the method for destroying Chicken object after getting 
     void RemoveAfterHit()
     {
         Destroy(this.gameObject);
     }
 }
+
+/// ideas: 1-sound invoke of getting close to chicken, volume getting louder??
+/// 2-Need to connect chicken with endgame if hit. 
